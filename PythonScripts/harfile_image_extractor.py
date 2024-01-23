72dc7cf1430abf4b563b8744d7b8c02807581f53
Shugu <cristiancsete@yahoo.com>
2024-01-23 19:08:45 +0000

    Refactor of the harfile_image_extractor

--- PythonScripts/harfile_image_extractor.py
+++ PythonScripts/harfile_image_extractor.py
@@ -4,28 +4,29 @@ import PIL
 from PIL import Image
 from io import BytesIO
 
+ENTRIES = json.load(open('JSONFiles/harfile.json'))['log']['entries']
+COUNTER = 100 # 100 so that they are ordered from the beginning when saved in the folder
+SAVE_PATH = 'Images/'
 
-entries = json.load(open('JSONFiles/harfile.json'))['log']['entries']
+def Image_saver(raw_image):
+    global COUNTER
+    # Create an image object from the decoded data
+    try:
+        image = Image.open(BytesIO(raw_image))
+        image.save(SAVE_PATH + str(COUNTER) + '.png')
 
-encrypted_list = []
+        COUNTER += 1
+    except PIL.UnidentifiedImageError:
+        print('Error: the image could not be identified')
 
-counter = 100
-
-for entry in entries:
+def response_entry(entry):
     res_content = entry['response']['content']
-    if 'image' in res_content['mimeType']:
-        if res_content['text']:
-            raw_image = base64.b64decode(res_content['text'])
-            
-            # Create an image object from the decoded data
-            try:
-                image = Image.open(BytesIO(raw_image))
-            
-                # Save the image to an Images folder
-                image.save('Images/' + str(counter) + '.png')
-                counter += 1
 
-            except PIL.UnidentifiedImageError:
-                print('Error: the image could not be identified')
+    if 'image' in res_content['mimeType'] and res_content['text']:
+        raw_image = base64.b64decode(res_content['text'])
+        Image_saver(raw_image)
+
+if __name__ == '__main__':
+    list(map(response_entry, ENTRIES))
 
 ### Current issue is that the images are not neccesarily in the right order because of the requests not being in order but there's no way to fix that. They will be more or less in the same order though.
