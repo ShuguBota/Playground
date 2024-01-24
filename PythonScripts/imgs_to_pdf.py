51a704c6d0c2454f74db718dcfee7643e13fc61f
Shugu <cristiancsete@yahoo.com>
2024-01-24 23:04:23 +0000

    Refactored the function to add images to the pdf by having a functional programming way of mapping through a function which adds each image in the list

--- PythonScripts/imgs_to_pdf.py
+++ PythonScripts/imgs_to_pdf.py
@@ -9,18 +9,24 @@ img_dir = 'Images/'
 # Create a PdfWriter object
 pdf_writer = PyPDF2.PdfWriter()
 
-for img in os.listdir(img_dir):
-    if img.endswith('.png'):
-        # Convert the image to PDF
-        img_path = os.path.join(img_dir, img)
-        img_pdf = img2pdf.convert(img_path)
+def add_page(img_name):
+    global pdf_writer
+    # Convert the image to PDF
+    img_path = os.path.join(img_dir, img_name)
+    img_pdf = img2pdf.convert(img_path)
 
-        # Create a PdfFileReader object from the image PDF
-        img_reader = PyPDF2.PdfReader(BytesIO(img_pdf))
+    # Create a PdfFileReader object from the image PDF
+    img_reader = PyPDF2.PdfReader(BytesIO(img_pdf))
 
-        # Add the page (which is the entire image) to the PdfWriter
-        pdf_writer.add_page(img_reader.pages[0])
+    # Add the page (which is the entire image) to the PdfWriter
+    pdf_writer.add_page(img_reader.pages[0])
 
-# Write the pages to a new PDF
-with open('Output/merged.pdf', 'wb') as f:
-    pdf_writer.write(f)
+
+
+if __name__ == '__main__':
+    imgs = filter(lambda x: x.endswith('.png'), os.listdir(img_dir))
+    list(map(add_page, imgs))
+
+    # Write the pages to a new PDF
+    with open('Output/merged.pdf', 'wb') as f:
+        pdf_writer.write(f)
