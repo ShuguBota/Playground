d8936478592dc81c2ed995238995765908deb91f
Shugu <cristiancsete@yahoo.com>
2024-01-25 11:08:56 +0000

    Refactored the code for kr_play to have OOP principles.

--- PythonScripts/kr_play.py
+++ PythonScripts/kr_play.py
@@ -1,40 +1,55 @@
 import random
 import json
 
-# List of Korean basic characters and their English translations
-simple_characters = json.load(open('JSONFiles/kr_simple_characters.json', 'r', encoding='utf-8'))
+class KoreanLearningProgram:
+    def __init__(self):
+        # List of Korean basic characters and their English translations
+        self.simple_characters = json.load(open('JSONFiles/kr_simple_characters.json', 'r', encoding='utf-8'))
+        self.characters_stack = list(self.simple_characters.keys())
+        self.wrong_characters_stack = []
 
-characters_stack = list(simple_characters.keys())
-random.shuffle(characters_stack)
-
-wrong_characters_stack = []
-
-
-if __name__ == "__main__":
-    print("Welcome to the Korean learning program!")
-    print("You will be shown a random Korean character and you will have to guess the English (Romanised) translation.")
-
-    while(True):
+    def game_over(self):
         # No more characters left
-        if len(characters_stack) == 0 and len(wrong_characters_stack) == 0:
+        if len(self.characters_stack) == 0 and len(self.wrong_characters_stack) == 0:
             print("Congratulations! You have completed the program!")
-            break
+            return True
+        
+        return False
 
+    def get_character(self):
         # If the stack is empty, shuffle the wrong characters stack
-        if len(characters_stack) == 0:
-            random.shuffle(wrong_characters_stack)
-            characters_stack = wrong_characters_stack
-            wrong_characters_stack = []
+        if len(self.characters_stack) == 0:
+            random.shuffle(self.wrong_characters_stack)
+            self.characters_stack = self.wrong_characters_stack
+            self.wrong_characters_stack = []
 
         # Get a character from the stack
-        character = characters_stack.pop()
+        return self.characters_stack.pop()
 
-        # Prompt the user to guess the English translation
-        guess = input(f"What is the English translation of '{character}'? ")
-
+    def check_answer(self, character, guess):
         # Check if the guess is correct
-        if guess.lower() == simple_characters[character]:
+        if guess.lower() == self.simple_characters[character]:
             print("Correct!")
         else:
-            print(f"Wrong! The correct translation is '{simple_characters[character]}'")
-            wrong_characters_stack.append(character)
+            print(f"Wrong! The correct translation is '{self.simple_characters[character]}'")
+            self.wrong_characters_stack.append(character)
+
+    def run(self):
+        print("Welcome to the Korean learning program!")
+        print("You will be shown a random Korean character and you will have to guess the English (Romanised) translation.")
+
+        random.shuffle(self.characters_stack)
+
+        while(True):
+            if self.game_over():
+                break
+
+            character = self.get_character()
+
+            # Prompt the user to guess the English translation
+            guess = input(f"What is the English translation of '{character}'? ")
+
+            self.check_answer(character, guess)
+
+if __name__ == "__main__":
+    KoreanLearningProgram().run()
